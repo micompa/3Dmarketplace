ea5b598025c11c5aaf8ece210eb530c97f41aee2
Julian Arango <ju1i4n@hotmail.com>
2022-10-28 20:07:32 -0500

    configuracion inical para el back y el front

---
+++ front/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tienda de Novedades en 3D</h1>
+      <img src ="./images/vetyshop.png" alt = "Aqui va el Logo"></img>
+          </div>
+  );
+}
+
+export default App;
