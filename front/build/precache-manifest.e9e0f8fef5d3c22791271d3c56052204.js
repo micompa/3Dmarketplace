dbe59edf54ef06f51fb9663e5432c3a73fa2d1df
Julian Arango <ju1i4n@hotmail.com>
2022-11-27 11:50:08 -0500

    se saca /build del git ignore

---
+++ front/build/precache-manifest.e9e0f8fef5d3c22791271d3c56052204.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "749a0f65c69bb7877f13",
+    "url": "/static/css/main.9ec3385e.chunk.css"
+  },
+  {
+    "revision": "749a0f65c69bb7877f13",
+    "url": "/static/js/main.749a0f65.chunk.js"
+  },
+  {
+    "revision": "71bb66cecc8aa0d6542b",
+    "url": "/static/css/1.901eed50.chunk.css"
+  },
+  {
+    "revision": "71bb66cecc8aa0d6542b",
+    "url": "/static/js/1.71bb66ce.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "39aace2934d97e973b3430105e3c12fd",
+    "url": "/index.html"
+  }
+];
